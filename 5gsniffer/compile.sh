26fabc972cf2e3123f7b4fbe805518ac8862dcd1
Norbert <ludant.n@northeastern.edu>
2023-06-06 11:24:59 -0400

    Deleted unused proto files

--- 5gsniffer/compile.sh
+++ 5gsniffer/compile.sh
@@ -1,6 +1,6 @@
 mkdir -p build
 cd build
-export CXX=/usr/bin/clang++-14
-export CC=/usr/bin/clang-14
-cmake -DCMAKE_C_COMPILER=/usr/bin/clang-14 -DCMAKE_CXX_COMPILER=/usr/bin/clang++-14 ..
+export CXX=/usr/bin/clang++-12
+export CC=/usr/bin/clang-12
+cmake -DCMAKE_C_COMPILER=/usr/bin/clang-12 -DCMAKE_CXX_COMPILER=/usr/bin/clang++-12 ..
 make -j 8
