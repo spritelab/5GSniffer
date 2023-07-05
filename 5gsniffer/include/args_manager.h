fb1eee7f090293460c4de825a1bf7a279050e09e
Norbert <ludant.n@northeastern.edu>
2023-07-05 05:17:17 -0400

    Added copyright text. Renamed toml file.

--- 5gsniffer/include/args_manager.h
+++ 5gsniffer/include/args_manager.h
@@ -1,12 +1,14 @@
-/*
- * Copyright (c) 2021.
+/**
+ * Copyright 2022-2023 SpriteLab @ Northeastern University
  *
- * This program is free software: you can redistribute it and/or modify
+ * This file is part of 5GSniffer.
+ *
+ * 5GSniffer is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as
- * published by the Free Software Foundation, either version 3 of the
- * License, or (at your option) any later version.
+ * published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version.
  *
- * This program is distributed in the hope that it will be useful,
+ * 5GSniffer is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Affero General Public License for more details.
@@ -14,7 +16,10 @@
  * A copy of the GNU Affero General Public License can be found in
  * the LICENSE file in the top-level directory of this distribution
  * and at http://www.gnu.org/licenses/.
+ *
  */
+
+
 
 #ifndef ARGS_MANAGER_H
 #define ARGS_MANAGER_H
